4257d89ac211a20bc89fca24d79e27fff8d53ea3
steph <stephane.mg@gmail.com>
2023-10-29 05:23:13 +0100

    i

--- web/src/main/java/RestSample.java
+++ web/src/main/java/RestSample.java
@@ -1,11 +1,19 @@
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 @Path("sample")
 public class RestSample {
 
+    private static final Log logger = LogFactory.getLog(RestSample.class);
+
+
     @GET
     public String getId() {
+
+        logger.debug("we get the id");
         return "abcdefg";
     }
 }
